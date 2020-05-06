445df93e12302e8f9c2fbfeca5bfdb0cf50b2623
marcs16 <luis.edo1234@gmail.com>
2020-05-05 19:35:24 -0500

    Changing registerview and adding positions for job

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,34 @@
+module UsersHelper
+
+	def options_for_position
+    	[
+    		'Asistente Administrativo Y Financiero',
+			'Asistente Comercial Y Social',
+			'Asistente Técnico Y Ambiental',
+			'Asistente Técnico Y Ambiental Aseo',
+			'Auxiliar Administrativo',
+			'Auxiliar Comercial Y Social La Danta',
+			'Auxiliar Comercial Y Social Urbano',
+			'Auxiliar De Servicios Generales',
+			'Ayudante De Sostenimiento La Danta',
+			'Ayudante De Sostenimiento Urbano',
+			'Conductor Vehículo Aseo',
+			'Gerente General',
+			'Lector',
+			'Operador De Ptap',
+			'Operador De Ptap La Danta',
+			'Operador De Ptar Urbano',
+			'Operario  De  Disposición  Final',
+			'Operario De Barrido Manejo Y Aseo',
+			'Operario De Recolección',
+			'Practicante',
+			'Subgerente Técnico',
+			'Supernumerario Administrativo',
+			'Supernumerario Operativo La Danta',
+			'Supernumerario Operativo Urbano',
+			'Supervisor Conductor'
+    	]
+	end
+end
+
+
