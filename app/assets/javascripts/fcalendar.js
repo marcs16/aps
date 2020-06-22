d69bab193e854e832eac9bc0b7fdb2134887a739
marcs16 <luis.edo1234@gmail.com>
2020-06-22 15:03:00 -0500

    Calendar and tablesfull dinamyc translation

--- app/assets/javascripts/fcalendar.js
+++ app/assets/javascripts/fcalendar.js
@@ -1,18 +1,23 @@
 function eventCalendar() {
   return $('#calendar').fullCalendar({ 
+    
   	header: {
        left: 'prev,next today',
        center: 'title',
        right: 'month,agendaWeek,agendaDay'
     },
+    locale: $('#dropdownLanguage').data('abbr'),
   	events:'/events.json'
+
   });
+ 
 };
 function clearCalendar() {
   $('#calendar').fullCalendar('delete'); 
   $('#calendar').html('');
 };
 $(document).on('turbolinks:load', function(){
-  eventCalendar();  
+  eventCalendar();
+  
 });
 $(document).on('turbolinks:before-cache', clearCalendar);
