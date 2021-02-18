87c1a0b649a9d73f4071c1996b99ce86faf8f9f5
marcs16 <luis.edo1234@gmail.com>
2021-02-18 18:28:52 -0500

    change on login logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
  class WelcomeController < ApplicationController
   def index
-    if @current_user =nil
-      redirect_to users_index_path
-    else
+    if @current_user = nil
       redirect_to new_user_session_path
+    else
+      redirect_to users_index_path
     end
   end
 
