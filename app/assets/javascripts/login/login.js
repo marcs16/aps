a99d8c4399db7d9e0cfd0ffb97188f6aa65ae1e6
marcs16 <luis.edo1234@gmail.com>
2021-04-15 17:13:37 -0500

    -Setting up sign up password to text toggle button -Changed color for sign up views -changed afk time deadline until log out

--- app/assets/javascripts/login/login.js
+++ app/assets/javascripts/login/login.js
@@ -1,7 +1,20 @@
 /**
  * Variables
  */
-$(document).ready(function(){ 
+$(document).ready(function(){
+  $(".login-psd").click(function(event) {
+    event.preventDefault();
+    $(this).removeClass("fa fa-eye");
+    $(this).addClass("fa fa-eye-slash ");
+    var input = $("#user_password");
+    if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+    $(this).removeClass("fa fa-eye-slash");
+    $(this).addClass("fa fa-eye");
+  }
+});
     $('#signup-button').click(function(event) {
       $('.user_options-forms').removeClass('bounceRight').addClass('bounceLeft');
     });
